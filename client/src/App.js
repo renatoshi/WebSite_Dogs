dfed6092e9369c61bd9000af49f1f0744b0c569d
renatoshi <ralcedom@gmail.com>
2022-12-08 17:12:24 -0500

    fixError6

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import DogDetails from "./components/DogDetails";
 import Home from "./components/Home";
 import DogCreate from "./components/DogCreate";
 import axios from "axios";
-axios.default.baseURL = "http://localhost:3001";
+axios.default.baseURL = "http://localhost:3001/";
 
 
 function App() {
